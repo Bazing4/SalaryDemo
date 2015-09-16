7391e025f8eb33910576da50d8b90619a96548c3
bazing4 <marco2yw@hotmail.com>
2015-09-16 12:27:23 -0700

    First Commit

---
+++ src/salarydemo/Employee.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salarydemo;
+
+/**
+ *
+ * @author Marco
+ */
+public class Employee
+{
+   private String name;
+
+   /**
+      Constructs an employee with an empty name.
+   */
+   public Employee()
+   {
+      name = "";
+   }
+
+   /**
+      Sets the name of this employee.
+      @param employee_name the new name 
+   */
+   public void setName(String employeeName)
+   {
+      name = employeeName;
+   }
+
+   /**
+      Gets the name of this employee.
+      @return the name 
+   */
+   public String getName()
+   {
+      return name; 
+   }
+
+   /**
+      Computes the pay for one week of work.
+      @param hoursWorked the number of hours worked in the week
+      @return the pay for the given number of hours
+   */
+   public double weeklyPay(int hoursWorked)
+   {
+      return 0; 
+   }
+}
